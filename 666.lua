16e42f6d09bb3f78db54657a3bc6f9817a01a1eb
fXz114 <146962720+fXz114@users.noreply.github.com>
2023-11-10 21:43:27 +0800

    Update 666.lua

--- 666.lua
+++ 666.lua
@@ -1,7 +1,7 @@
-if game.PlaceId == 9872472334 then
+if game.PlaceId == 13772394625 then
 		loadstring(game:HttpGet("https://pastebin.com/raw/49pHahaW"))()
 end
 
-if game.PlaceId == 13772394625 then
+if game.PlaceId == 9872472334 then
 		loadstring(game:HttpGet("https://pastebin.com/raw/gD0ipYyi"))()
 end
